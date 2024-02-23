41a602d16ae60348dddb1a57dda9efeb554d87d6
pawan chimote <ankushchimote2001@gmail.com>
2024-02-23 12:29:45 +0530

    4

---
+++ app/src/App.jsx
@@ -0,0 +1,171 @@
+import { useEffect, useState } from 'react';
+import styled from 'styled-components'
+import SearchResult from './components/SearchResult/SearchResult';
+
+
+export const BASE_URL ="http://localhost:9000"  //export word was not there before, due to in searchresult.jsx line15 
+
+const App = () => {
+
+  const [data,setData] =useState(null)
+  const [filterData,setFilterData] = useState(null); //for search functionality,check line24
+  const [loading,setLoading] =useState(false)  // this is use to show that loading icon when page loading
+  const [error,setError] =useState(null)
+  const [selectedBtn, setSelectedBtn] = useState("all");
+
+  useEffect(() => {
+    const fetchFoodData =async () =>{
+      setLoading(true)
+  
+      try {
+        const response =await fetch(BASE_URL)
+        const json = await response.json()   //learn the meaning of async await again
+        
+        setData(json)
+        setFilterData(json)
+        setLoading(false)
+      } catch (error) {
+        setError("Unable to fetch data")
+      }
+      
+     }
+     fetchFoodData()
+  },[]) // if u just kept it empty it will load page only 1 time
+
+  const searchFood =(e) => {
+    const searchValue =e.target.value;
+    console.log(searchValue); //try to write in searchbox and see console
+
+    if(searchValue === ""){
+      setFilterData(null)
+    }
+
+    const filter = data?.filter((food) => {  ///here filter is a function just like tolowercase
+     return food.name.toLowerCase().includes(searchValue.toLowerCase())  ///here we wrote return but in video without return working fine
+    })
+    setFilterData(filter) //the data of filter function will be set in setFilterData
+  }
+
+  const filterFood = (type) => {
+    if (type === "all") {
+      setFilterData(data);
+      setSelectedBtn("all");
+      return;
+    }
+
+    const filter = data?.filter((food) =>
+      food.type.toLowerCase().includes(type.toLowerCase())
+    );
+    setFilterData(filter);
+    setSelectedBtn(type);
+  };
+
+  const filterBtns = [
+    {
+      name: "All",
+      type: "all",
+    },
+    {
+      name: "Breakfast",
+      type: "breakfast",
+    },
+    {
+      name: "Lunch",
+      type: "lunch",
+    },
+    {
+      name: "Dinner",
+      type: "dinner",
+    },
+  ];
+
+
+   if (error) return <div>{error}</div>
+   if (loading) return <div>loading...</div>
+
+  return(
+      <>
+           <Container>
+           <TopContainer>
+              <div className="logo">
+                <img src="./Foody Zone.png" alt="" />
+              </div>
+              <div className='search'>
+                <input
+                onChange={searchFood}
+                placeholder='Search Food'
+                />
+              </div>
+           </TopContainer>
+
+           <FilterContainer>
+          {filterBtns.map((value) => (
+            <Button
+              isSelected={selectedBtn === value.type}
+              key={value.name}
+              onClick={() => filterFood(value.type)}
+            >
+              {value.name}
+            </Button>
+          ))}
+        </FilterContainer>
+
+         
+        
+     </Container>
+     <SearchResult data={filterData}/>
+      </>
+  )
+};
+
+export default App;
+
+export const Container = styled.div`
+  max-width: 1200px;
+  margin: 0 auto;
+`
+const TopContainer =styled.section`
+  min-height: 140px;
+  display: flex;
+  justify-content: space-between;
+  padding: 16px;
+  align-items: center;
+
+  .search{
+    input{
+      background-color: transparent;
+      border: 1px solid red;
+      color: white;
+      border-radius: 5px;
+      height: 40px;
+      font-size: 16px;
+      padding: 0 10px;
+      &::placeholder {
+        color: white;
+      }
+    }
+  }
+  @media (0 < width < 600px) {
+    flex-direction: column;
+    height: 120px;
+  }
+`
+const FilterContainer =styled.section`
+  display: flex;
+  justify-content: center;
+  gap: 10px;
+  padding-bottom: 40px;
+`
+export const Button =styled.button`
+  background: ${({ isSelected }) => (isSelected ? "#f22f2f" : "#ff4343")};
+  outline: 1px solid ${({ isSelected }) => (isSelected ? "white" : "#ff4343")};
+  border-radius: 5px;
+  padding: 6px 12px;
+  border: none;
+  color: white;
+  cursor: pointer;
+  &:hover {
+    background-color: #f22f2f;
+  }
+`
+
---
+++ app/src/components/SearchResult/SearchResult.jsx
@@ -0,0 +1,104 @@
+import React from 'react'
+import styled from 'styled-components'
+import { BASE_URL } from '../../App'
+import { Button, Container } from '../../App' //this comming from app.jsx css
+
+const SearchResult = ({data}) => {  ///here we just changed name of data into food to avoid confusion
+  return (
+    <FoodCardContainer>
+    <Container>
+    <FoodCards>
+        {
+            data?.map(({name, image, text, price}) => 
+            <FoodCard     ///try to remove "?" and see console,   this ? check if we got data then render data and if we didnt get data then show nothing(null)
+            key={name}      //////try to understand this code from line 7 to 17
+            >
+                <div className="food_image">
+                    {/* uncomment the line 15 to see error */}
+                    {/* //<img src={food.image} />  if we run this and see console it will show error that image is on 9000 and u r on 5173 :::: to overcome this we exported BASE_URL */}
+                    <img src={BASE_URL + image} />
+                    </div>
+
+                    <div className="food_info">
+                        <div className="info">
+                            <h3>{name}</h3>
+                            <p>{text}</p>
+                        </div>
+                        <Button>${price.toFixed(2)}</Button>
+                        </div>    
+            </FoodCard>)
+
+            
+        }
+    </FoodCards>
+    </Container>
+  </FoodCardContainer>
+  )
+}
+
+export default SearchResult
+
+const FoodCardContainer =styled.section`
+  height: calc(100vh - 210px); // we use calc to eliminate scroll by subtracting
+  background-image: url('./bg.png');
+  background-size: cover;
+`
+const FoodCards = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  row-gap: 32px;
+  column-gap: 20px;
+  justify-content: center;
+  align-items: center;
+  padding-top: 80px;
+`
+const FoodCard = styled.div`
+   width: 340px;
+  height: 167px;
+  border: 0.66px solid;
+
+  border-image-source: radial-gradient(
+      80.69% 208.78% at 108.28% 112.58%,
+      #eabfff 0%,
+      rgba(135, 38, 183, 0) 100%
+    ),
+    radial-gradient(
+      80.38% 222.5% at -13.75% -12.36%,
+      #98f9ff 0%,
+      rgba(255, 255, 255, 0) 100%
+    );
+
+  background: url(.png),
+    radial-gradient(
+      90.16% 143.01% at 15.32% 21.04%,
+      rgba(165, 239, 255, 0.2) 0%,
+      rgba(110, 191, 244, 0.0447917) 77.08%,
+      rgba(70, 144, 213, 0) 100%
+    );
+  background-blend-mode: overlay, normal;
+  backdrop-filter: blur(13.1842px);
+
+  border-radius: 20px;
+
+  display: flex;
+  padding: 8px;
+
+  .food_info {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: end;
+    h3 {
+      margin-top: 8px;
+      font-size: 16px;
+      font-weight: 500;
+    }
+    p {
+      margin-top: 4px;
+      font-size: 12px;
+    }
+    button {
+      font-size: 12px;
+    }
+  }
+`
